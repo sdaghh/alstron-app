ffd80805be57d29ae791e6c3318b6b67c909d5d9
sdaghh <chuvvi4brunda@gmail.com>
2024-07-12 12:55:50 +0530

    changyyyyy1

--- src/test/java/com/training/bms/AlstromAppApplicationTests.java
+++ src/test/java/com/training/bms/AlstromAppApplicationTests.java
@@ -2,12 +2,15 @@ package com.training.bms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class AlstromAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(191, 200);
 	}
+	
 
 }
